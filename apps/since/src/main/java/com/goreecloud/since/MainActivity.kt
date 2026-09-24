70be235e58e434878d7a54a13fcf0dfdae1864a8
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-24 03:04:47 -0500

    Align Since system bars with light and dark themes

--- apps/since/src/main/java/com/goreecloud/since/MainActivity.kt
+++ apps/since/src/main/java/com/goreecloud/since/MainActivity.kt
@@ -3,12 +3,14 @@ package com.goreecloud.since
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.goreecloud.since.ui.SinceApp
 import com.goreecloud.since.ui.theme.SinceTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val sinceApplication = application as SinceApplication
 
         setContent {
