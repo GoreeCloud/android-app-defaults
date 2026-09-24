e9cb2916616d6884e6ff5f38e25ee8f3be759021
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-23 21:40:29 -0500

    Wire Since database and repository

--- apps/since/src/main/java/com/goreecloud/since/SinceApplication.kt
+++ apps/since/src/main/java/com/goreecloud/since/SinceApplication.kt
@@ -1,5 +1,19 @@
 package com.goreecloud.since
 
 import android.app.Application
+import com.goreecloud.since.data.local.SinceDatabaseFactory
+import com.goreecloud.since.data.repository.RoomTrackerRepository
+import java.time.Clock
 
-class SinceApplication : Application()
+class SinceApplication : Application() {
+    val database by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        SinceDatabaseFactory.build(this)
+    }
+
+    val trackerRepository by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        RoomTrackerRepository(
+            dao = database.trackerDao(),
+            clock = Clock.systemUTC(),
+        )
+    }
+}
