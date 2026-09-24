de80c9dea08c872635de95b504c1bee7fa8c7437
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-24 17:15:13 -0500

    Add Since preferences DataStore

--- apps/since/build.gradle.kts
+++ apps/since/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
     implementation("androidx.compose.ui:ui:1.8.2")
     implementation("androidx.compose.foundation:foundation:1.8.2")
     implementation("androidx.compose.material3:material3:1.3.2")
