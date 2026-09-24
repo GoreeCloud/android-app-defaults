1d43fbc25845c70c7ef6a6dcaf7c265b81712af2
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-23 22:31:48 -0500

    Inject Since repository into app UI

--- apps/since/src/main/java/com/goreecloud/since/MainActivity.kt
+++ apps/since/src/main/java/com/goreecloud/since/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.goreecloud.since.ui.SinceApp
 import com.goreecloud.since.ui.theme.SinceTheme
+import java.time.Clock
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val sinceApplication = application as SinceApplication
+
         setContent {
             SinceTheme {
-                SinceApp()
+                SinceApp(
+                    repository = sinceApplication.trackerRepository,
+                    clock = Clock.systemUTC(),
+                )
             }
         }
     }
